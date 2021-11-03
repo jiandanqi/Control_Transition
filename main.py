dc71ab1044c1b0a1f686d7cb1205a361b63785c0
highd01 <zjq1998@outlook.com>
2021-11-03 22:43:57 +0800

    读取

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import sys
 import pickle
 import argparse
 
+from data_management.read_csv import *
 
-
-if __name__ == '__main_' :
+def create_args():
+    parser = argparse.ArgumentParser(description="Parameter")
+    # --- Input paths ---
+    parser.add_argument('--input_path', default="E:/21_10_Control Transition/data/01_tracks.csv", type=str,
+                        help='CSV file of the tracks')
+    parsed_arguments = vars(parser.parse_args())
+    return parsed_arguments
+    print(args.input_path)
+#if __name__ == '__main_' :
+ #   create_args()

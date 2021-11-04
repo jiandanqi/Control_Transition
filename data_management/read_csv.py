6477816dc4e13d678b25b0b63736bab6602935c4
highd01 <zjq1998@outlook.com>
2021-11-04 19:16:50 +0800

    读取

--- data_management/read_csv.py
+++ data_management/read_csv.py
@@ -1,6 +1,25 @@
 import pandas
 import numpy as np
 import os
-
-file_name = os.listdir(r'E:/21_10_Control Transition/data')
-print(file_name)
+for i in range(1,61):
+    str1 = str(i)
+    path = str1.zfill(2) + '_tracks'
+    print(path)
+    pathname= 'E:/21_10_Control Transition/data/' + path + '.csv'
+    print(pathname)
+    df = pandas.read_csv(pathname)
+    #get the columns of df
+    df.columns=['frame','id','x','y','width','height','xVelocity',
+            'yVelocity','xAcceleration','yAcceleration','frontSightDistance',
+            'backSightDistance','dhw','thw','ttc','precedingXVelocity',
+            'precedingId','followingId','leftPrecedingId','leftAlongsideId',
+            'leftFollowingId','rightPrecedingId','rightAlongsideId','rightFollowingId','laneId'
+            ]
+    c=list(df.columns)
+    print(path)
+    print(c[1:2])
+    group=df.groupby(c[1:2])
+    for key, df in group:
+        print(key)
+        print(df)
+    print(group)
--- judge_way/judge_way.py
+++ judge_way/judge_way.py
@@ -1,5 +1,8 @@
 import numpy as np
+import math
 
 from data_management.read_csv import *
 
-def f(x,y):
+def f(x,y):  #车辆航向角
+    ()
+
